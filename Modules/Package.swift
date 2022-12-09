d10f4eaf6c3798bbec066cfcd09301e2657ebfdc
dingle <dingle@dialpad.com>
2022-12-09 10:25:08 -0800

    Add APIClient module

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -6,12 +6,14 @@ import PackageDescription
 let package = Package(
     name: "Modules",
     products: [
+        .library(name: "APIClient", targets: ["APIClient"]),
         .library(name: "SharedModels", targets: ["SharedModels"]),
     ],
     dependencies: [
 
     ],
     targets: [
+        .target(name: "APIClient", dependencies: []),
         .target(name: "SharedModels", dependencies: []),
         .testTarget(name: "SharedModelsTests", dependencies: ["SharedModels"]),
     ]
