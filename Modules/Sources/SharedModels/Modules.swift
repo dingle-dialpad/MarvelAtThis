509e2144ecafea6991f76d638de1f819673d166b
dingle <dingle@dialpad.com>
2022-12-09 10:21:40 -0800

    Add SPM manifest file

---
+++ Modules/Sources/SharedModels/Modules.swift
@@ -0,0 +1,11 @@
+//
+// Created by Dave Ingle
+// Copyright © 2022 Dialpad Inc. All rights reserved.
+//
+
+public struct Modules {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Modules/Tests/SharedModelsTests/ModulesTests.swift
@@ -0,0 +1,16 @@
+//
+// Created by Dave Ingle
+// Copyright © 2022 Dialpad Inc. All rights reserved.
+//
+
+import XCTest
+@testable import SharedModels
+
+final class SharedModelsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Modules().text, "Hello, World!")
+    }
+}
