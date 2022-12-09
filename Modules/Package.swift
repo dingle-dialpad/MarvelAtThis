c530dd621359e5771fb6b629e7589dd265044cae
dingle <dingle@dialpad.com>
2022-12-09 10:40:48 -0800

    Add TCA dependency to Package

--- Modules/Package.swift
+++ Modules/Package.swift
@@ -5,16 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "Modules",
+    platforms: [.macOS(.v10_15), .iOS(.v15)],
     products: [
         .library(name: "APIClient", targets: ["APIClient"]),
         .library(name: "SharedModels", targets: ["SharedModels"]),
     ],
     dependencies: [
-
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: Version("0.47.2"))
     ],
     targets: [
-        .target(name: "APIClient", dependencies: []),
+        .target(name: "APIClient", dependencies: [.tca]),
         .target(name: "SharedModels", dependencies: []),
         .testTarget(name: "SharedModelsTests", dependencies: ["SharedModels"]),
     ]
 )
+
+extension Target.Dependency {
+    static let tca: Target.Dependency = .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+}
